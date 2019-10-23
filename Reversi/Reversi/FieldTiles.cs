e0d94e1508514512656bd83d8b8beb79d70ed15b
Jorden Bakker <j.bakker6@students.uu.nl>
2019-10-23 14:36:19 +0200

    changes

---
+++ Reversi/Reversi/FieldTiles.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace Reversi
+{
+
+    public class FieldTiles : UserControl
+    {
+        private System.ComponentModel.Container components = null;
+
+        // Default color values.
+        
+        public static readonly Color NormalBackColorDefault = Color.Green;
+
+        // Colors used in rendering the control.
+        public static Color NormalBackColor = NormalBackColorDefault;
+
+        
+
+        // Used for animation.
+        public static readonly int AnimationStart = 6;
+        public static readonly int AnimationStop = FieldTiles.AnimationStart;
+        public int AnimationCounter = FieldTiles.AnimationStop;
+
+        // These reflect the position of the square on the board.
+        public int Row
+        {
+            get { return this.row; }
+        }
+        public int Col
+        {
+            get { return this.col; }
+        }
+
+        // These reflect the public row and column properties.
+        private int row;
+        private int col;
+
+        // Drawing tools.
+        private static Pen pen = new Pen(Color.Black);
+        
+
+        public FieldTiles(int row, int col)
+        {
+            
+            InitializeComponent();
+
+
+            this.row = row;
+            this.col = col;
+
+
+            // Set the background color.
+            this.BackColor = FieldTiles.NormalBackColor;
+
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+
+        
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            
+            this.Name = "SquareControl";
+            this.Size = new Size(32, 32);
+            //this.Load += new EventHandler(this.FieldTiles_Load);
+            this.Paint += new PaintEventHandler(this.FieldTiles_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+       
+        private void FieldTiles_Paint(object sender, PaintEventArgs e)
+        {
+            // Painting it the normal color
+            Color backColor = FieldTiles.NormalBackColor;
+           
+
+            e.Graphics.Clear(backColor);
+
+
+            // Draw the border of the field
+            Point topLeft = new Point(0, 0);
+            Point topRight = new Point(this.Width - 1, 0);
+            Point bottomLeft = new Point(0, this.Height - 1);
+            Point bottomRight = new Point(this.Width - 1, this.Height - 1);
+            FieldTiles.pen.Width = 1;
+            e.Graphics.DrawLine(FieldTiles.pen, bottomLeft, topLeft);
+            e.Graphics.DrawLine(FieldTiles.pen, topLeft, topRight);
+            
+            e.Graphics.DrawLine(FieldTiles.pen, bottomLeft, bottomRight);
+            e.Graphics.DrawLine(FieldTiles.pen, bottomRight, topRight);
+
+            
+        }
+
+        /*private void FieldTiles_Load(object sender, EventArgs e)
+        {
+
+        }*/
+    }
+}
---
+++ Reversi/Reversi/ReversiForm.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Reversi;
+public partial class ReversiForm : Form
+{
+    public Button newGame, help;
+    public Panel p;
+    public Label blueScore, redScore, gameState;
+    int xRedScore = 300;
+    int yRedScore = 20;
+    int xBlueScore = 880;
+    int yBlueScore = 20;
+    int yBoxMax = 5;
+    int xBoxMax = 5;
+    int xPanel = 300;
+    int yPanel = 100;
+    int xPanelSize = 800;
+    int yPanelSize = 400;
+    public Rectangle r = new Rectangle();
+
+    //Adding the playing field
+    private Field field;
+
+    public ReversiForm()
+    {
+        //Creating the buttons
+        newGame = new Button();
+        help = new Button();
+        newGame.Location = new Point(350, 550);
+        help.Location = new Point(900, 550);
+        newGame.Size = new Size(100, 75);
+        help.Size = new Size(100, 75);
+        newGame.Text = "New Game";
+        help.Text = "Help";
+
+        //Creating the panel in which the game will be played
+        p = new Panel();
+        p.Location = new Point(xPanel, yPanel);
+        p.ClientSize = new Size(xPanelSize, yPanelSize);
+
+        //Creating the labels
+        blueScore = new Label();
+        redScore = new Label();
+        gameState = new Label();
+        blueScore.Location = new Point(xBlueScore, yBlueScore);
+        redScore.Location = new Point(xRedScore, yRedScore);
+        gameState.Location = new Point(580, 20);
+        blueScore.Size = new Size(100, 13);
+        redScore.Size = new Size(100, 13);
+        gameState.Size = new Size(100, 13);
+        blueScore.Text = "Blue Score: 0";
+        redScore.Text = "Red Score: 0";
+        gameState.Text = "State of the game here";
+
+        //Adding all elements to the program
+        Controls.Add(newGame);
+        Controls.Add(help);
+        Controls.Add(p);
+        Controls.Add(blueScore);
+        Controls.Add(redScore);
+        Controls.Add(gameState);
+        
+    }
+
+}
